514c8935c9225a1889ab617c9e633a43b7169f28
pallabez <pallabsonowal999@gmail.com>
2021-06-06 01:51:01 +0530

    Added Quick Sort and made header file to sort out common functions.

---
+++ header.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <random>
+#include <windows.h>
+
+void scrambleInputLinear(int input[], int size) {
+    std::default_random_engine eng;
+    
+    for(int i = 0; i < size; i++) {
+        input[i] = i + 1;
+    }
+
+    for(int i = 0; i < size * 2; i++) {
+        
+        int random = eng() % size;
+        int random2 = eng() % size;
+
+        int temp = input[random];
+        input[random] = input[random2];
+        input[random2] = temp;
+    }
+}
+
+void printState(int input[], int size) {
+    system("cls");
+    
+    for(int i = 0; i < size; i++) {
+        for(int j = 0; j < input[i]; j++) {
+            std::cout << "*";
+        }
+        std::cout << "\n";
+    }
+}
+
+void displayArray(int input[], int size) {
+    for(int i = 0; i < size; i++) {
+        std::cout << input[i] << " ";
+    }
+    std::cout << "\n";
+}
+
+void swap(int input[], int index1, int index2) {
+    int temp = input[index2];
+    input[index2] = input[index1];
+    input[index1] = temp;
+}
---
+++ quickSort.cpp
@@ -0,0 +1,44 @@
+#include "header.cpp"
+
+void displayQuickSort(int input[], int size, int fullInput[], int fullSize) {
+    if(size <= 1) return;
+
+    int pivot = input[0];
+    int pivotIndex{};
+
+    for(int i = 1; i < size; i++) {
+        if(input[i] < pivot) pivotIndex++;
+    }
+
+    if(pivotIndex != 0) {
+        swap(input, pivotIndex, 0);
+        Sleep(1000);
+        printState(fullInput, fullSize);
+    }
+
+    for(int i = 0, j = pivotIndex + 1; i < pivotIndex && j < size;) {
+        if(input[i] >= pivot && input[j] < pivot) {
+            swap(input, i, j);
+            Sleep(1000);
+            printState(fullInput, fullSize);
+        }
+        else {
+            if(input[i] < pivot) i++;
+            if(input[j] > pivot) j++;
+        }
+    }
+
+    displayQuickSort(input, pivotIndex, fullInput, fullSize);
+    displayQuickSort(input + pivotIndex + 1, size - pivotIndex - 1, fullInput, fullSize);
+}
+
+int main() {
+    int size;
+    std::cin >> size;
+
+    int *input = new int[size];
+
+    scrambleInputLinear(input, size);
+    displayQuickSort(input, size, input, size);
+    printState(input, size);
+}
